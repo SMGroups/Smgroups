f8518b21efb07d039228125526d812115e53f4f9
SMGroups <thesmgroups@gmail.com>
2025-01-27 23:44:56 +0530

    Initial commit

--- server/index.js
+++ server/index.js
@@ -1,40 +1,72 @@
 const express = require('express');
-// const mongoose = require('mongoose');
+const cors = require('cors');
 const messageRoutes = require('./routes/contact');
 require('dotenv').config();
 
 const app = express();
-const cors = require('cors');
+
+// CORS configuration
 const allowedOrigins = ["https://smgroups.vercel.app"];
-app.options("*", cors());
 app.use(
   cors({
-    origin: allowedOrigins,
-    methods: ["GET", "POST", "PUT", "DELETE"], // Allowed HTTP methods
-    credentials: true, // Allow cookies if required
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
   })
 );
 
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cors());
+app.options("*", cors());
 
-// Define routes
+// Routes
 app.use('/api', messageRoutes);
 
-// mongoose.connect(process.env.DB_URI, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// })
-//   .then(() => {
-//     console.log('MongoDB connected');
-//   })
-//   .catch(err => {
-//     console.error('MongoDB connection error:', err);
-//   });
-
+// Server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+
+
+// const express = require('express');
+// // const mongoose = require('mongoose');
+// const messageRoutes = require('./routes/contact');
+// require('dotenv').config();
+
+// const app = express();
+// const cors = require('cors');
+
+// const allowedOrigins = ["https://smgroups.vercel.app"];
+// app.use(
+//   cors({
+//     origin: allowedOrigins,
+//     methods: ["GET", "POST", "PUT", "DELETE"], // Allowed HTTP methods
+//     credentials: true, // Allow cookies if required
+//   })
+// );
+
+
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// app.options("*", cors());
+
+// // Define routes
+// app.use('/api', messageRoutes);
+
+// // mongoose.connect(process.env.DB_URI, {
+// //   useNewUrlParser: true,
+// //   useUnifiedTopology: true,
+// // })
+// //   .then(() => {
+// //     console.log('MongoDB connected');
+// //   })
+// //   .catch(err => {
+// //     console.error('MongoDB connection error:', err);
+// //   });
+
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
