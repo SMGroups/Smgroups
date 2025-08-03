fc346cb9172d986aa1ff1120794f0e7f5ebd7f65
SMGroups <thesmgroups@gmail.com>
2025-08-03 15:39:59 +0530

    Update mailService.js

--- server/services/mailService.js
+++ server/services/mailService.js
@@ -9,7 +9,7 @@ const sendEmail = async (data) => {
     secure: false,
     auth: {
         user: process.env.EMAIL,
-        pass: process.env.PASS
+        pass: 'qfdizmkpufoffwxg'
     }
   });
 
